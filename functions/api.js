508bdcbb85806c2efa7ea996d0b4aab9b518b114
Kiran944 <deepkiran0081@gmail.com>
2024-07-29 15:48:27 +0530

    commit

--- functions/api.js
+++ functions/api.js
@@ -1,27 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const connectDb = require('./db/connect');
 const cookieParser = require('cookie-parser');
-const userRouter = require('./routes/users');
-const reportsRouter = require('./routes/reports');
-const messageRouter = require('./routes/message');
-const notificationRouter = require('./routes/notification');
+const connectDb = require('../db/connect');
+const userRouter = require('../routes/users');
+const reportsRouter = require('../routes/reports');
+const messageRouter = require('../routes/message');
+const notificationRouter = require('../routes/notification');
+const serverless = require('serverless-http');
 
 const app = express();
 app.use(express.json());
 
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://master--beamish-arithmetic-22778c.netlify.app'
+    'https://bugtrackingwebsite.netlify.app/'
 ];
 
 app.use(cors({
     credentials: true,
     origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
-
         if (allowedOrigins.indexOf(origin) === -1) {
             const msg = 'The CORS policy for this site does not allow access from the specified origin.';
             return callback(new Error(msg), false);
@@ -39,7 +38,7 @@ const PORT = process.env.PORT || 8080;
 connectDb();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Server Live<h1/>');
+    res.send('<h1>Server Live</h1>');
 });
 
 app.use('/users', userRouter);
@@ -47,6 +46,4 @@ app.use('/report', reportsRouter);
 app.use('/message', messageRouter);
 app.use('/notification', notificationRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
-});
+module.exports.handler = serverless(app);
