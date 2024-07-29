523d1a899ffd0cd251272f57e2e3536e517b3ba7
Kiran944 <deepkiran0081@gmail.com>
2024-07-29 16:34:13 +0530

    commit

--- server.js
+++ server.js
@@ -1,52 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const connectDb = require('./db/connect');
-const cookieParser = require('cookie-parser');
-const userRouter = require('./routes/users');
-const reportsRouter = require('./routes/reports');
-const messageRouter = require('./routes/message');
-const notificationRouter = require('./routes/notification');
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const connectDb = require('./db/connect')
+const cookieParser = require('cookie-parser')
+const userRouter = require('./routes/users')
+const reportsRouter = require('./routes/reports')
+const messageRouter = require('./routes/message')
+const notificationRouter = require('./routes/notification')
 
-const app = express();
-app.use(express.json());
-
-const allowedOrigins = [
-    'http://localhost:3000',
-    'https://master--beamish-arithmetic-22778c.netlify.app'
-];
-
+const app = express()
+app.use(express.json())
 app.use(cors({
     credentials: true,
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
+    origin: 'http://localhost:3000'
+}))
+app.use(cookieParser())
+app.use(express.urlencoded({extended: false}))
 
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+const PORT = process.env.PORT || 8000;
 
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+//connect to mongodb database
+connectDb()
 
-const PORT = process.env.PORT || 8080;
+app.get('/', (req,res) => {
+    res.send('<h1>Server Live<h1/>')
+})
+app.use("/users", userRouter)
+app.use("/report", reportsRouter)
+app.use("/message", messageRouter)
+app.use("/notification", notificationRouter)
 
-// Connect to MongoDB database
-connectDb();
-
-app.get('/', (req, res) => {
-    res.send('<h1>Server Live<h1/>');
-});
-
-app.use('/users', userRouter);
-app.use('/report', reportsRouter);
-app.use('/message', messageRouter);
-app.use('/notification', notificationRouter);
-
-app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
-});
+app.listen(process.env.PORT || PORT , () => {
+    console.log(`Server Running on ${process.env.PORT}`);
+}) 
