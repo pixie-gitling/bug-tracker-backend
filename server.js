61c2726971a286cc60985a9b5f9b3dd29394028a
Kiran944 <deepkiran0081@gmail.com>
2024-07-29 16:47:28 +0530

    commit

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const connectDb = require('./db/connect')
-const cookieParser = require('cookie-parser')
-const userRouter = require('./routes/users')
-const reportsRouter = require('./routes/reports')
-const messageRouter = require('./routes/message')
-const notificationRouter = require('./routes/notification')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const connectDb = require('./db/connect');
+const cookieParser = require('cookie-parser');
+const userRouter = require('./routes/users');
+const reportsRouter = require('./routes/reports');
+const messageRouter = require('./routes/message');
+const notificationRouter = require('./routes/notification');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
 const allowedOrigins = ['http://localhost:3000', 'https://bugtrackingwebsite.netlify.app'];
 
 app.use(cors({
     credentials: true,
     origin: function (origin, callback) {
+        console.log(`Incoming origin: ${origin}`);  // Debugging log
+
         // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
@@ -24,22 +27,24 @@ app.use(cors({
         return callback(null, true);
     }
 }));
-app.use(cookieParser())
-app.use(express.urlencoded({extended: false}))
+
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.PORT || 8000;
 
-//connect to mongodb database
-connectDb()
+// Connect to MongoDB database
+connectDb();
 
-app.get('/', (req,res) => {
-    res.send('<h1>Server Live<h1/>')
-})
-app.use("/users", userRouter)
-app.use("/report", reportsRouter)
-app.use("/message", messageRouter)
-app.use("/notification", notificationRouter)
+app.get('/', (req, res) => {
+    res.send('<h1>Server Live</h1>');
+});
 
-app.listen(process.env.PORT || PORT , () => {
-    console.log(`Server Running on ${process.env.PORT}`);
-}) 
+app.use('/users', userRouter);
+app.use('/report', reportsRouter);
+app.use('/message', messageRouter);
+app.use('/notification', notificationRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server Running on port ${PORT}`);
+});
